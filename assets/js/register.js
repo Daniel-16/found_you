1dcd7076b04b1904477445fcb693801b349c0053
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2022-11-21 00:55:58 +0100

    bug fix

--- assets/js/register.js
+++ assets/js/register.js
@@ -9,7 +9,7 @@ form.addEventListener("submit", (e) => {
     localStorage.setItem("userLong", position.coords.longitude);
   }
   function errorOnPosition() {
-    console.error("Could not get your current location")
+    console.error("Could not get your current location");
   }
   if (navigator.geolocation) {
     navigator.geolocation.watchPosition(showPosition, errorOnPosition);
@@ -31,6 +31,7 @@ form.addEventListener("submit", (e) => {
       })
       .then((data) => {
         console.log(data);
+        location.replace("find.html");
       })
       .catch((err) => {
         console.error(err);
