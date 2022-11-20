4f0f19a0f2ca97b156f3429bf14e412cce2e1f7e
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2022-11-21 00:50:04 +0100

    feat: register page separated

---
+++ assets/js/register.js
@@ -0,0 +1,39 @@
+var form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const phoneNumber = document.getElementById("phoneNumber").value;
+  const password = document.getElementById("password").value;
+  function showPosition(position) {
+    localStorage.setItem("userLat", position.coords.latitude);
+    localStorage.setItem("userLong", position.coords.longitude);
+  }
+  function errorOnPosition() {
+    console.error("Could not get your current location")
+  }
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(showPosition, errorOnPosition);
+    fetch("https://phone-tracker.onrender.com/api/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        phoneNumber,
+        password,
+        userLat: localStorage.getItem("userLat"),
+        userLong: localStorage.getItem("userLong"),
+      }),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+});
